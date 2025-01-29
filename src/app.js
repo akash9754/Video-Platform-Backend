18d613312dade934e8e7871c7fc1b56742d7cd23
akash <akashvishwakarma0602@gmail.com>
2025-01-29 07:05:11 +0530

    add database connection setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import express from "express";
 
+
+// mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+
+
 const app = express();
 
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
 import app from "./app.js";
 import dotenv from "dotenv/config"
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDb from "./db/dbConnection.js";
+import { error } from "console";
 
 const Port = process.env.PORT || 3000
 
-app.listen(Port, () => {
-    console.log("hi i am listening on ", Port);
-    
-})
+
+connectDb()
+.then(
+    app.listen(Port, () =>{
+        console.log(`server is available on port : ${Port}`);
+    })
+)
+.catch(
+    (error) => {
+        console.error("database connection error", error);
+    }
+)
+
+
+
+
+
+// connect database using try catch
+
+ /*
+;(async () => {
+    try {
+       const connection = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on('error', (error) => {
+            console.log("error", error);
+            throw error
+        })
+
+        app.listen(Port, () => {
+            console.log("hi i am listening on ", Port);    
+        })
+
+        
+    } catch (error) {
+        console.error("Database Connection Error :", error.message);
+        throw error
+        
+    }
+})()
+ */
