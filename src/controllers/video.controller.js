ba0f283e0cc15a76b1b759d1c080bea497e275fc
akash <akashvishwakarma0602@gmail.com>
2025-09-04 15:50:33 +0530

    video file

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -7,5 +7,7 @@ import { uploadOnCloudinary } from "../utils/cloudinary";
 
 const getAllVideos = asyncHandler ( async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+
+    Video.find({})
     
 })
